c025766b22ee9a7b8cc8010e9b955cd971b216d5
Sam Chaffee <sam@moodlerooms.com>
2013-10-22 20:38:34 -0600

    INT-5422: Code review revisions

--- settings.php
+++ settings.php
@@ -27,21 +27,21 @@ if ($ADMIN->fulltree) {
     require_once($CFG->dirroot.'/blocks/profile_redirect/lib.php');
     $configs = array();
 
-    $configs[] = new admin_setting_heading('profiledesc', get_string('description', 'block_profile_redirect'),
-            get_string('descriptiontxt', 'block_profile_redirect'));
+    $configs[] = new admin_setting_heading('profiledesc', new lang_string('description', 'block_profile_redirect'),
+            new lang_string('descriptiontxt', 'block_profile_redirect'));
 
-    $configs[] = new admin_setting_configselect('profilefield', get_string('profilefield', 'block_profile_redirect'),
-            get_string('profilefielddesc', 'block_profile_redirect'), 'institution', block_profile_redirect_profile_fields());
+    $configs[] = new admin_setting_configselect('profilefield', new lang_string('profilefield', 'block_profile_redirect'),
+            new lang_string('profilefielddesc', 'block_profile_redirect'), 'institution', block_profile_redirect_profile_fields());
 
-    $options = array('shortname' => get_string('shortname'),
-                     'fullname'  => get_string('fullname'),
-                     'idnumber'  => get_string('idnumber'));
+    $options = array('shortname' => new lang_string('shortname'),
+                     'fullname'  => new lang_string('fullname'),
+                     'idnumber'  => new lang_string('idnumber'));
 
-    $configs[] = new admin_setting_configselect('coursefield', get_string('coursefield', 'block_profile_redirect'),
-            get_string('coursefielddesc', 'block_profile_redirect'), 'shortname', $options);
+    $configs[] = new admin_setting_configselect('coursefield', new lang_string('coursefield', 'block_profile_redirect'),
+            new lang_string('coursefielddesc', 'block_profile_redirect'), 'shortname', $options);
 
-    $configs[] = new admin_setting_configtext('defaultcourse', get_string('defaultcourse', 'block_profile_redirect'),
-            get_string('defaultcoursedesc', 'block_profile_redirect'),'', PARAM_TEXT);
+    $configs[] = new admin_setting_configtext('defaultcourse', new lang_string('defaultcourse', 'block_profile_redirect'),
+            new lang_string('defaultcoursedesc', 'block_profile_redirect'),'', PARAM_TEXT);
 
     foreach ($configs as $config) {
         $config->plugin = 'blocks/profile_redirect';
