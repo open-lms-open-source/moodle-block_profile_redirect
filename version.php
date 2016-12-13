aa973e106bb76cf092b9c13c0b6d3cc0cd8fd39c
Mark Nielsen <mark@moodlerooms.com>
2016-12-13 14:00:26 -0800

    INT-10252: bump plugin version, requires and release for Moodle 3.1

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version   = 2016012600;
-$plugin->requires  = 2015111604; // Requires this Moodle version (3.0.4).
+$plugin->version   = 2016121300;
+$plugin->requires  = 2016052300;
 $plugin->component = 'block_profile_redirect';
-$plugin->release   = '3.0.4 (Build: 20160509)';
+$plugin->release   = '3.1.3';
 $plugin->maturity  = MATURITY_STABLE;
