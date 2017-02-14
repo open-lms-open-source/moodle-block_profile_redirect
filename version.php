17c27bd347fa44c5bc5fa09ea9508ac9cbb6942b
Mark Nielsen <mark@moodlerooms.com>
2017-02-14 08:55:17 -0800

    INT-11133: bump requires and release for Moodle 3.2.1

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 
 $plugin->version   = 2016121300;
-$plugin->requires  = 2016052300;
+$plugin->requires  = 2016120500;
 $plugin->component = 'block_profile_redirect';
-$plugin->release   = '3.1.3';
+$plugin->release   = '3.2.1';
 $plugin->maturity  = MATURITY_STABLE;
