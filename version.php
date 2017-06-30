6207acde9da2f66b9c33f23c4b654f095d366f8f
Mark Nielsen <mark@moodlerooms.com>
2017-06-30 09:08:54 -0700

    INT-11942: Bump version and release

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version   = 2016121300;
+$plugin->version   = 2017063000;
 $plugin->requires  = 2016120500;
 $plugin->component = 'block_profile_redirect';
-$plugin->release   = '3.2.2';
+$plugin->release   = '3.2.3';
 $plugin->maturity  = MATURITY_STABLE;
