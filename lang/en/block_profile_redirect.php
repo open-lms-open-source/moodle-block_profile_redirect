ff61185a5a3313ae70f626bb3fdf3ccc8a67f4f5
Rafael Becerra <rafael.becerrarodriguez@blackboard.com>
2018-09-11 14:51:15 +0000

    INT-13653: Change the rebrand of the copyright url's from http://www.blackboardopenlms.com to http://www.blackboard.com

--- lang/en/block_profile_redirect.php
+++ lang/en/block_profile_redirect.php
@@ -19,7 +19,7 @@
  *
  * @package    block_profile_redirect
  * @author     Jason Hardin, Chris Stones, and others.
- * @copyright  Copyright (c) 2013 Blackboard Inc. (http://www.blackboardopenlms.com)
+ * @copyright  Copyright (c) 2013 Blackboard Inc. (http://www.blackboard.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
