759471dda57cf34adb6e119208818702477309f8
David Castro <david.castro@openlms.net>
2021-02-08 06:47:46 +0000

    INT-16713: Importing it lang

--- lang/it/block_profile_redirect.php
+++ lang/it/block_profile_redirect.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2020 Blackboard Inc. (http://www.blackboard.com)
+ * @copyright  Copyright (c) 2021 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -23,24 +23,26 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'Reindirizzamento profilo';
 $string['blockname'] = 'Reindirizzamento profilo';
-$string['description'] = 'Descrizione';
+$string['description'] = 'Commento';
 $string['descriptiontxt'] = '<p>Il block Reindirizzamento profilo è progettato per reindirizzare un utente dalla pagina home di un sito o dalla pagina Il mio Moodle a
- un nuovo corso.  Il nuovo corso al quale viene indirizzato l\'utente si basa su un valore presente in un campo profilo corrispondente a un valore nel nome completo, nome abbreviato o numero ID del corso secondo le configurazioni del sito sottostanti.</p>
+un nuovo corso. Il nuovo corso al quale viene indirizzato l\'utente si basa su un valore presente in un campo profilo corrispondente a un valore nel nome completo, nome abbreviato o numero ID del corso secondo le configurazioni del sito sottostanti.</p>
 
 <p>Ricorda che dopo aver configurato le impostazioni di block, l\'amministratore deve aggiungere un\'istanza del block nella pagina home o nella pagina Il
-mio Moodle.  Per fare ciò, completa i seguenti passaggi:
+mio Moodle. Per fare ciò, completa i seguenti passaggi:
 <ol>
 <li>Vai alla pagina home, abilita le modifiche e seleziona Reindirizzamento profilo dal menu a discesa del block "Aggiungi un block".</li>
 <li>Dopo aver aggiunto il block, tutti gli utenti vengono reindirizzati al corso specificato nel loro campo profilo o al corso predefinito se non sono presenti valori validi nel loro profilo.</li>
 </ol>
- </p>';
+</p>';
 $string['profilefield'] = 'Campo profilo';
-$string['profilefielddesc'] = 'Questo è il campo presente in un profilo utente che il block Reindirizzamento profilo utilizzerà per ottenere una corrispondenza con il campo del corso.';
+$string['profilefielddesc'] = 'Questo è il campo presente in un profilo utente che il block Reindirizzamento profilo utilizzerà per ottenere una corrispondenza
+con il campo del corso.';
 $string['coursefield'] = 'Campo corso';
-$string['coursefielddesc'] = 'Questo è il campo presente nelle impostazioni del corso che il block Reindirizzamento profilo utilizzerà per ottenere una corrispondenza con il campo profilo dell\'utente per il reindirizzamento.';
+$string['coursefielddesc'] = 'Questo è il campo presente nelle impostazioni del corso che il block Reindirizzamento profilo utilizzerà per ottenere una corrispondenza
+con il campo profilo dell\'utente per il reindirizzamento.';
 $string['defaultcourse'] = 'Corso predefinito';
 $string['defaultcoursedesc'] = 'Questo è il corso al quale verranno reindirizzati tutti gli utenti non amministratori dopo la visualizzazione del block, nel caso in cui non dispongano
- di un valore valido nel loro campo profilo.  Questo campo deve avere un nome completo, un nome abbreviato o un numero ID del corso basato sul valore nel menu a discesa del campo del corso';
+di un valore valido nel loro campo profilo. Questo campo deve avere un nome completo, un nome abbreviato o un numero ID del corso basato sul valore nel menu a discesa del campo del corso';
 $string['sysadmin'] = 'Questo block è visibile solo per gli amministratori del sito';
 $string['profile_redirect:addinstance'] = 'Aggiungi un nuovo block Reindirizzamento profilo';
 $string['profile_redirect:myaddinstance'] = 'Aggiungi un\'istanza del block Reindirizzamento profilo alla mia pagina';
