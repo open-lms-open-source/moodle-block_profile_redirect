103e7f59689741fc88a094c1e17542a3f89f7de1
Fabian Batioja <fabian.batioja@openlms.net>
2021-06-01 22:14:04 +0000

    INT-17116: Bump versions for 3.10

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version   = 2020061100;
-$plugin->requires  = 2020061500;
+$plugin->version   = 2021060100;
+$plugin->requires  = 2020110900;
 $plugin->component = 'block_profile_redirect';
-$plugin->release   = '3.9.3';
+$plugin->release   = '3.10.3';
 $plugin->maturity  = MATURITY_STABLE;
