cd3da232d13697835e9371755485e25c07d9c2bb
Daniel Cifuentes <daniel.cifuentes@openlms.net>
2023-03-22 20:11:35 +0000

    INT-18815: Importing tr lang

--- lang/tr/block_profile_redirect.php
+++ lang/tr/block_profile_redirect.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2022 Open LMS (https://www.openlms.net)
+ * @copyright  Copyright (c) 2023 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -24,26 +24,26 @@ defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Profil Yönlendirmesi';
 $string['blockname'] = 'Profil Yönlendirmesi';
 $string['description'] = 'Açıklama';
-$string['descriptiontxt'] = '<p>Profil Yönlendirmesi bloku, bir kullanıcıyı sitenin ön sayfasından veya bloku içeren Moodle’ım sayfasından
-yeni bir kursa yönlendirmek için tasarlanmıştır. Kullanıcının yönlendirildiği yeni kurs, profil alanında aşağıdaki site yapılandırmalarına göre kurs tam adı, kısa adı veya kimlik numarasındaki bir değerle eşleşen bir değeri temel alır.</p>
+$string['descriptiontxt'] = '<p>Profil Yönlendirmesi bloğu, kullanıcıyı site ön sayfasından veya bloku içeren Moodle sayfasından yeni bir kursa yönlendirmek için
+tasarlanmıştır. Kullanıcının yönlendirildiği yeni kurs, aşağıdaki site yapılandırmalarına göre profil alanında kursun tam adı, kısa adı veya kimlik numarası ile eşleşen bir değere göre belirlenir.</p>
 
-<p>Blok ayarları yapılandırıldıktan sonra bir yöneticinin blok örneğini ön sayfaya veya
-Moodle’ım sayfasına eklemesi gerektiğini lütfen unutmayın. Bunu yapmak için, aşağıdaki adımları tamamlayın:
+<p>Blok ayarları yapılandırıldıktan sonra yöneticinin blokun bir örneğini ön sayfaya veya My Moodle sayfasına eklemesi gerektiğini unutmayın.
+Bunun için aşağıdaki adımları izleyin:
 <ol>
-<li>Ön sayfaya gidin, düzenlemeyi etkinleştirin ve "Blok Ekle" blok açılır listesinden Profil Yönlendirme’yi seçin.</li>
-<li>Blok eklendikten sonra tüm kullanıcılar, profil alanlarında belirtilen kursa veya profillerinde bir değer yoksa
+<li>Ön sayfaya gidin, düzenlemeyi etkinleştirin ve "Blok Ekle" açılır menüsünden Profil Yönlendirme öğesini seçin.</li>
+<li>Blok eklendikten sonra tüm kullanıcılar profil alanında belirtilen kursa veya profillerinde geçerli bir değer yoksa
 varsayılan kursa yönlendirilir.</li>
 </ol>
 </p>';
 $string['profilefield'] = 'Profil Alanı';
-$string['profilefielddesc'] = 'Bu, bir kullanıcının profilinde Profil Yönlendirme blokunun kurs alanıyla
-eşleştirmek için kullanacağı alandır.';
+$string['profilefielddesc'] = 'Bu, bir kullanıcının profilinde Profil Yönlendirme blokunun kurs alanıyla eşleştirmek
+için kullanacağı alandır.';
 $string['coursefield'] = 'Kurs Alanı';
 $string['coursefielddesc'] = 'Bu, kurs ayarlarında Profil Yönlendirme blokunun yönlendirme için kullanıcının profil alanıyla
 eşleştirmek için kullanacağı alandır.';
 $string['defaultcourse'] = 'Varsayılan Kurs';
 $string['defaultcoursedesc'] = 'Bu, yönetici olmayan kullanıcıların bloka baktıktan sonra profil alanlarında geçerli bir değer yoksa
-yönlendirileceği kurstur. Bu alan, kurs alanı açılan listesindeki değere göre kursun kısa adını, tam adını veya kurs kimlik numarasını içermelidir';
+yönlendirileceği kurstur. Bu alan, kurs alanı açılır listesindeki değere göre kursun kısa adını, tam adını veya kurs kimlik numarasını içermelidir';
 $string['sysadmin'] = 'Bu bloku yalnızca Site Yöneticileri görebilir';
 $string['profile_redirect:addinstance'] = 'Yeni bir Profil Yönlendirme bloku ekle';
 $string['profile_redirect:myaddinstance'] = 'Profil Yönlendirme blokunun bir örneğini sayfama ekle';
