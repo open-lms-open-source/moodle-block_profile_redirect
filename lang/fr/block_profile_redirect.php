e2c21129276f9fcd0d6f9a06f094b675e93a6f98
Felipe Vargas <felipe.vargas@openlms.net>
2024-05-28 13:21:38 +0000

    INT-19523: Importing fr lang

--- lang/fr/block_profile_redirect.php
+++ lang/fr/block_profile_redirect.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2023 Open LMS (https://www.openlms.net)
+ * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -24,26 +24,19 @@ defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Redirection de profil';
 $string['blockname'] = 'Redirection de profil';
 $string['description'] = 'Description';
-$string['descriptiontxt'] = '<p>Le bloc Redirection de profil est conçu pour rediriger un utilisateur de la page d\'accueil du site ou de la page Mon Moodle contenant le bloc vers
-un nouveau cours. Le nouveau cours vers lequel l\'utilisateur est redirigé est basé sur une valeur dans un champ de profil correspondant à une valeur dans le nom complet, le nom court ou le numéro d\'identification du cours, selon les configurations du site ci-dessous.</p>
+$string['descriptiontxt'] = '<p>Le bloc de Redirection de profil est conçu pour rediriger un utilisateur de la page d’accueil du site ou de la page Mon Moodle qui contient le bloc vers un nouveau cours. Le nouveau cours vers lequel l\'utilisateur est redirigé est basé sur une valeur dans un champ de profil correspondant à une valeur dans le nom complet, le nom court ou le numéro d\'identification du cours, selon les configurations du site ci-dessous.</p>
 
-<p>N\'oubliez pas qu\'une fois les paramètres de bloc configurés, un administrateur doit ajouter une instance du bloc à la première page ou à la page Mon
-Moodle. Pour ce faire, procédez comme suit :
-<ol>
-<li>Accédez à la première page, activez la modification et sélectionnez Redirection de profil dans la liste déroulante de blocs « Ajouter un bloc ».</li>
-<li>Une fois le bloc ajouté, tous les utilisateurs seront redirigés vers le cours spécifié dans leur champ de profil ou vers le cours par défaut si
-aucune valeur valide ne figure dans leur profil.</li>
+<p>N\'oubliez pas qu\'une fois les paramètres de bloc configurés, un administrateur doit ajouter une instance du bloc à la première page ou à la page Mon Moodle. Pour ce faire, suivez ces étapes :<ol>
+<li>Accédez à la première page, activez la modification et sélectionnez Redirection de profil dans la liste déroulante de blocs « Ajouter un bloc ».</li>
+<li>Une fois le bloc ajouté, tous les utilisateurs seront redirigés vers le cours spécifié dans leur champ de profil ou vers le cours par défaut si aucune valeur valide n\'est présente dans leur profil.</li>
 </ol>
 </p>';
 $string['profilefield'] = 'Champ du profil';
-$string['profilefielddesc'] = 'Il s\'agit du champ que le bloc Redirection de profil utilise dans un profil utilisateur pour déterminer le
-champ du cours.';
+$string['profilefielddesc'] = 'Il s\'agit du champ que le bloc Redirection de profil utilise dans un profil utilisateur pour déterminer le champ du cours.';
 $string['coursefield'] = 'Champ du cours';
-$string['coursefielddesc'] = 'Il s\'agit du champ que le bloc Redirection de profil utilise dans les paramètres de cours et fait correspondre au champ du profil
-utilisateur pour réaliser la redirection.';
+$string['coursefielddesc'] = 'Il s\'agit du champ que le bloc Redirection de profil utilise dans les paramètres de cours et fait correspondre au champ du profil utilisateur pour réaliser la redirection.';
 $string['defaultcourse'] = 'Cours par défaut';
-$string['defaultcoursedesc'] = 'Il s\'agit du cours vers lequel tous les utilisateurs non-administrateurs sont redirigés lorsqu\'ils affichent le bloc et qu\'ils n\'ont
-aucune information valide dans le champ de leur profil. Ce champ doit contenir le nom complet, le nom abrégé ou l\'identifiant du cours, en fonction de la valeur figurant dans la liste déroulante du champ du cours.';
+$string['defaultcoursedesc'] = 'Il s\'agit du cours vers lequel tous les utilisateurs non-administrateurs sont redirigés lorsqu\'ils affichent le bloc et qu\'ils n\'ont aucune information valide dans le champ de leur profil. Ce champ doit contenir le nom complet, le nom abrégé ou l\'identifiant du cours, en fonction de la valeur figurant dans la liste déroulante du champ du cours.';
 $string['sysadmin'] = 'Ce bloc ne s\'affiche que pour les administrateurs du site';
 $string['profile_redirect:addinstance'] = 'Ajouter un nouveau bloc Redirection de profil';
 $string['profile_redirect:myaddinstance'] = 'Ajouter une instance de bloc Redirection de profil à Ma page';
