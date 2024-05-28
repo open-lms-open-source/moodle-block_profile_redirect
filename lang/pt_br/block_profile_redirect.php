06f49c83ed1b8040c4cd77337f88410adde1eff1
Felipe Vargas <felipe.vargas@openlms.net>
2024-05-28 13:22:21 +0000

    INT-19523: Importing pt_br lang

--- lang/pt_br/block_profile_redirect.php
+++ lang/pt_br/block_profile_redirect.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2023 Open LMS (https://www.openlms.net)
+ * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -24,26 +24,19 @@ defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Redirecionar perfil';
 $string['blockname'] = 'Redirecionar perfil';
 $string['description'] = 'Texto do link';
-$string['descriptiontxt'] = '<p>O bloco Redirecionar perfil foi projetado para redirecionar um usuário da página inicial do site ou da página Meu Moodle que contém o bloco para
-um novo curso. O novo curso para o qual o usuário é redirecionado baseia-se em um valor em um campo de perfil que corresponde a um valor no nome completo do curso, nome abreviado ou número de ID com base nas configurações do site abaixo.</p>
+$string['descriptiontxt'] = '<p>O bloco Redirecionar perfil é projetado para redirecionar um usuário da página inicial do site ou da página Meu Moodle contendo o bloco para um novo curso. O novo curso para o qual o usuário é redirecionado baseia-se em um valor em um campo de perfil que corresponde a um valor no nome completo do curso, nome curto ou número de ID com base nas configurações do site abaixo.</p>
 
-<p>Lembre-se de que, uma vez que as configurações do bloco são definidas, um administrador precisa adicionar uma instância do bloco à página inicial ou à página
-Meu Moodle. Para fazer isso, execute as seguintes etapas:
-<ol>
-<li>Navegue até a página inicial, habilite a edição e selecione Redirecionar perfil na lista suspensa do bloco &quot;Adicionar um bloco&quot;.</li>
-<li>Depois que o bloco for adicionado, todos os usuários serão redirecionados para o curso especificado em seu campo de perfil ou para o curso padrão se
-nenhum valor válido estiver em seu perfil.</li>
+<p>Lembre-se de que, uma vez que as configurações do bloco são definidas, um administrador precisa adicionar uma instância do bloco à página inicial ou à página Meu Moodle. Para fazer isso, conclua as seguintes etapas:<ol>
+<li>Navegue até a página inicial, habilite a edição e selecione Redirecionar perfil na lista suspensa do bloco "Adicionar um bloco".</li>
+<li>Depois que o bloco for adicionado, todos os usuários serão redirecionados para o curso especificado em seu campo de perfil ou para o curso padrão se nenhum valor válido estiver em seu perfil.</li>
 </ol>
 </p>';
 $string['profilefield'] = 'Campo do perfil';
-$string['profilefielddesc'] = 'Esse é o campo no perfil de usuário que o bloco Redirecionar perfil usará para corresponder ao
-campo do curso.';
+$string['profilefielddesc'] = 'Esse é o campo no perfil de usuário que o bloco Redirecionar perfil usará para fazer a correspondência em relação ao campo do curso.';
 $string['coursefield'] = 'Campo do curso';
-$string['coursefielddesc'] = 'Esse é o campo nas configurações do curso que o bloco Redirecionar perfil usará para corresponder com o
-perfil do usuário para redirecionamento.';
+$string['coursefielddesc'] = 'Esse é o campo nas configurações do curso que o bloco Redirecionar perfil usará fazer correspondência com o campo do perfil de usuário para redirecionamento.';
 $string['defaultcourse'] = 'Curso padrão';
-$string['defaultcoursedesc'] = 'Este é o curso para o qual todos os usuários não administradores serão direcionados ao visualizar o bloco se eles não
-tiverem um valor válido no campo de perfil. Este campo deve ter o nome abreviado, nome completo ou número de ID do curso com base no valor na lista suspensa do campo do curso';
+$string['defaultcoursedesc'] = 'Este é o curso para o qual todos os usuários não administradores serão direcionados após visualizar o bloco, caso eles não tenham um valor válido em seu campo do perfil. Esse campo deve ter o nome curto, o nome completo ou o número do ID do curso baseados no valor do menu suspenso do campo do curso';
 $string['sysadmin'] = 'Esse bloco só é exibido para administradores de site';
 $string['profile_redirect:addinstance'] = 'Adicionar um novo bloco de Redirecionamento do perfil';
 $string['profile_redirect:myaddinstance'] = 'Adicionar uma instância de bloco de Redirecionamento do perfil para a minha página';
