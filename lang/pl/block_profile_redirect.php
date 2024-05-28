90972781534fd423d66f07dc2359aef502e92063
Felipe Vargas <felipe.vargas@openlms.net>
2024-05-28 13:21:56 +0000

    INT-19523: Importing pl lang

--- lang/pl/block_profile_redirect.php
+++ lang/pl/block_profile_redirect.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2023 Open LMS (https://www.openlms.net)
+ * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -24,26 +24,19 @@ defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Przekierowanie profilu';
 $string['blockname'] = 'Przekierowanie profilu';
 $string['description'] = 'Opis';
-$string['descriptiontxt'] = '<p>Blok Przekierowanie profilu przeznaczony jest to przekierowywania użytkownika ze strony głównej witryny lub Mojej strony Moodle, zawierającej blok,
-do nowego kursu. Nowy kurs, na który użytkownik zostaje przekierowany, wybierany jest na podstawie wartości w polu profilu, która odpowiada wartości pełnej nazwy, krótkiej nazwy lub identyfikatora kursu w zależności od poniższych konfiguracji.</p>
+$string['descriptiontxt'] = '<p>Blok Przekierowanie profilu ma na celu przekierowywanie użytkownika ze strony głównej witryny lub strony My Moodle zawierającej blok do nowego kursu. Nowy kurs, na który użytkownik zostaje przekierowany, wybierany jest na podstawie wartości w polu profilu, która odpowiada wartości pełnej nazwy, krótkiej nazwy lub identyfikatora kursu w zależności od poniższych konfiguracji.</p>
 
-<p>Należy pamiętać, że po skonfigurowaniu ustawień bloku administrator musi dodać instancje bloku do strony głównej lub Mojej strony
-Moodle. W tym celu należy wykonać następujące kroki:
-<ol>
+<p>Należy pamiętać, że po skonfigurowaniu ustawień bloku administrator musi dodać instancję bloku do strony głównej lub strony My Moodle. W tym celu należy wykonać poniższe kroki:<ol>
 <li>Przejdź do strony głównej, włącz edytowanie i wybierz Przekierowanie profilu z rozwijanego menu „Dodaj blok”.</li>
-<li>Po dodaniu bloku wszyscy użytkownicy będą przekierowywani do kursu określonego w polu profilu lub do domyślnego kursu, jeśli
-ich profil nie zawiera poprawnej wartości.</li>
+<li>Po dodaniu bloku wszyscy użytkownicy będą przekierowywani do kursu określonego w polu profilu lub do domyślnego kursu, jeśli w ich profilu nie ma prawidłowej wartości.</li>
 </ol>
 </p>';
 $string['profilefield'] = 'Pole profilu';
-$string['profilefielddesc'] = 'Pole w profilu użytkownika, z którego skorzysta blok Przekierowanie profilu, aby dopasować jego wartość do wartości
-pola kursu.';
+$string['profilefielddesc'] = 'Pole w profilu użytkownika, z którego skorzysta blok Przekierowanie profilu, aby dopasować jego wartość do wartości pola kursu.';
 $string['coursefield'] = 'Pole kursu';
-$string['coursefielddesc'] = 'Pole w ustawieniach kursu, z którego skorzysta blok Przekierowanie profilu, aby dopasować jego wartość do
-wartości pola profilu użytkownika w celu przekierowania.';
+$string['coursefielddesc'] = 'Pole w ustawieniach kursu, z którego skorzysta blok Przekierowanie profilu, aby dopasować jego wartość do wartości pola profilu użytkownika w celu przekierowania.';
 $string['defaultcourse'] = 'Domyślny kurs';
-$string['defaultcoursedesc'] = 'Kurs, do którego zostaną przekierowani wszyscy użytkownicy niebędący administratorami po wyświetleniu bloku, w przypadku gdy
-ich pole profilu nie zawiera poprawnej wartości. To pole powinno zawierać pełną nazwę, skróconą nazwę lub identyfikator kursu w zależności od wartości określonej w menu rozwijanym pola kursu.';
+$string['defaultcoursedesc'] = 'Kurs, do którego zostaną przekierowani wszyscy użytkownicy niebędący administratorami po wyświetleniu bloku, w przypadku gdy ich pole profilu nie zawiera poprawnej wartości. To pole powinno zawierać pełną nazwę, skróconą nazwę lub identyfikator kursu w zależności od wartości określonej w menu rozwijanym pola kursu.';
 $string['sysadmin'] = 'Ten blok mogą wyświetlić wyłącznie administratorzy witryny';
 $string['profile_redirect:addinstance'] = 'Dodaj nowy blok Przekierowanie profilu';
 $string['profile_redirect:myaddinstance'] = 'Dodaj instancję bloku Przekierowanie profilu do mojej strony';
