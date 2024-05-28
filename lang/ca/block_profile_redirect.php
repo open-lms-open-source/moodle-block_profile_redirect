e52206320861c09a384f714db373d476a6c81693
Felipe Vargas <felipe.vargas@openlms.net>
2024-05-28 13:22:16 +0000

    INT-19523: Importing ca lang

--- lang/ca/block_profile_redirect.php
+++ lang/ca/block_profile_redirect.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2023 Open LMS (https://www.openlms.net)
+ * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -24,26 +24,19 @@ defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Redirecció de perfil';
 $string['blockname'] = 'Redirecció de perfil';
 $string['description'] = 'Descripció';
-$string['descriptiontxt'] = '<p>La funció del bloc de redirecció de perfil és redirigir l\'usuari des de la pàgina principal del lloc o una pàgina El meu Moodle que contingui el bloc a
-un curs nou. Aquest curs nou on es redirigeix l\'usuari es basa en si un valor del camp de perfil coincideix amb un valor del nom complet, el nom curt o el número d\'ID d\'un curs, segons les configuracions del lloc que es mostren a continuació.</p>
+$string['descriptiontxt'] = '<p>El bloc de redirecció del perfil s\'ha dissenyat per redirigir un usuari des de la pàgina principal del lloc o la pàgina "El meu Moodle" que conté el bloc a un curs nou. El curs nou a què es redirigeix l\'usuari es basa en un valor en un camp de perfil que coincideixi amb un valor en el nom complet del curs, nom curt o número d\'identificació basat en les configuracions del lloc a continuació.</p>
 
-<p>Tingueu en compte que, una vegada es configuren els paràmetres del bloc, l\'administrador ha d\'afegir una instància del bloc a la pàgina principal o a la pàgina
-El meu Moodle. Per fer-ho, seguiu els passos següents:
-<ol>
-<li>Aneu a la pàgina principal, activeu l\'edició i, al bloc desplegable &quot;Afegeix un bloc&quot;, seleccioneu Redirecció de perfil.</li>
-<li>Un cop afegit el bloc, es redirigiran tots els usuaris al curs especificat al seu camp de perfil, o bé al curs predeterminat si
-no tenen cap valor vàlid al perfil.</li>
+<p>Tingueu en compte que, una vegada es configuren els paràmetres del bloc, l\'administrador ha d\'afegir una instància del bloc a la pàgina principal o a la pàgina "El meu Moodle". Per fer això, seguiu aquests passos:<ol>
+<li>Aneu a la pàgina principal, activeu l\'edició i, al bloc desplegable "Afegeix un bloc", seleccioneu Redirecció de perfil.</li>
+<li>Un cop afegit el bloc, es redirigiran tots els usuaris al curs especificat al seu camp de perfil, o bé al curs predeterminat si no hi ha cap valor vàlid al seu perfil.</li>
 </ol>
 </p>';
 $string['profilefield'] = 'Camp de perfil';
-$string['profilefielddesc'] = 'Aquest és el camp al perfil de l\'usuari que el bloc de Redirecció de perfil compararà amb el
-camp del curs.';
+$string['profilefielddesc'] = 'Aquest és el camp del perfil de l\'usuari que el bloc de Redirecció de perfil compararà amb el camp del curs.';
 $string['coursefield'] = 'Camp del curs';
-$string['coursefielddesc'] = 'Aquest és el camp dels paràmetres del curs que el bloc de Redirecció de perfil compararà amb el
-camp de perfil de l\'usuari per establir la redirecció.';
+$string['coursefielddesc'] = 'Aquest és el camp dels paràmetres del curs que el bloc de Redirecció de perfil compararà amb el camp del perfil de l\'usuari per establir la redirecció.';
 $string['defaultcourse'] = 'Curs predeterminat';
-$string['defaultcoursedesc'] = 'Si no hi ha un valor vàlid al camp de perfil, els usuaris no administradors es redirigiran a aquest curs
-en visualitzar el bloc. En aquest camp, hi hauria d\'haver el nom curt, el nom complet i el número d\'ID del curs segons el valor que apareix al menú desplegable del camp del curs';
+$string['defaultcoursedesc'] = 'Si no hi ha un valor vàlid al camp de perfil, els usuaris no administradors es redirigiran a aquest curs en visualitzar el bloc. En aquest camp, hi hauria d\'haver el nom curt, el nom complet i el número d\'identificació del curs segons el valor que apareix al menú desplegable del camp del curs.';
 $string['sysadmin'] = 'Només els administradors del lloc poden visualitzar aquest bloc.';
 $string['profile_redirect:addinstance'] = 'Afegeix un nou bloc de redirecció de perfil';
 $string['profile_redirect:myaddinstance'] = 'Afegeix una instància de bloc de redirecció de perfil a la pàgina El meu Moodle';
