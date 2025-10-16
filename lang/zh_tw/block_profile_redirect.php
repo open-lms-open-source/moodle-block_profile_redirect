b19da19f5514ff9ac83ddff1eeb84229ca2000f9
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2025-10-16 21:37:09 +0000

    INT-21443: Importing zh_tw lang

--- lang/zh_tw/block_profile_redirect.php
+++ lang/zh_tw/block_profile_redirect.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
+ * @copyright  Copyright (c) 2025 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -24,19 +24,26 @@ defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = '資訊重新引導';
 $string['blockname'] = '資訊重新引導';
 $string['description'] = '描述';
-$string['descriptiontxt'] = '<p>「資訊重新引導」區塊用於將使用者從包含該區塊的網站首頁或「我的 Moodle」頁面重新引導至新課程。使用者被重新引導到的新課程是根據資訊欄位中的值與依據下方網站配置之課程全名、簡稱或編號值相符。</p>
+$string['descriptiontxt'] = '<p>「資訊重新引導」區塊目的在從網站首頁或包含區塊之我的 Moodle 頁面重新引導使用者到
+新的課程。使用者被重新引導到的新課程是根據資訊欄位中的值與依據下方網站配置之課程全名、簡稱或編號值相符。</p>
 
-<p>請記住，區塊設定配置完成後，管理員需要將區塊的例項新增至首頁或「我的 Moodle」頁面。要做到這點，請完成下列步驟：<ol>
+<p>請記得，一旦區塊設定已配置，管理員需要將區塊的例項新增至首頁或我的
+Moodle 頁面。若要做到這點，請完成下列步驟：
+<ol>
 <li>瀏覽至首頁，啟用編輯，並從「新增區塊」區塊下拉式清單中選取「資訊重新引導」。</li>
-<li>新增區塊後，所有使用者都將被重新引導至其資訊欄位中指定的課程，或如果其資訊中沒有有效的值，則將重新引導至預設課程。</li>
+<li>新增區塊後，所有使用者將被重新引導至其資訊欄位中指定的課程，或如果其資訊中沒有有效的值，
+則會重新引導至預設課程。</li>
 </ol>
 </p>';
 $string['profilefield'] = '資訊欄位';
-$string['profilefielddesc'] = '這個是使用者資訊裡的欄位，「資訊重新引導」區塊將用它來跟課程欄位進行配對。';
+$string['profilefielddesc'] = '這個是使用者資訊裡的欄位，「資訊重新引導」區塊將用它來跟
+課程欄位進行配對。';
 $string['coursefield'] = '課程欄位';
-$string['coursefielddesc'] = '這個是課程設定裡的欄位，「資訊重新引導」區塊將用它來跟使用者的資訊欄位進行配對，然後進行重新引導。';
+$string['coursefielddesc'] = '這個是課程設定裡的欄位，「資訊重新引導」區塊將用它來跟
+使用者的資訊欄位進行配對，然後進行重新引導。';
 $string['defaultcourse'] = '預設課程';
-$string['defaultcoursedesc'] = '若使用者的身分不是管理員，且其資訊欄位中的值無效，則當檢視此區塊時，會由系統引導至此課程。依據課程欄位下拉式清單裡的值，這個欄位應該有課程簡稱、全名或編號號碼';
+$string['defaultcoursedesc'] = '若使用者的身分不是管理員，且其資訊欄位沒有有效的值，則當檢視此區塊時，
+會引導至此課程。依據課程欄位下拉式清單裡的值，這個欄位應該有課程簡稱、全名或課程編號號碼';
 $string['sysadmin'] = '只有網站管理員能看見此區塊';
 $string['profile_redirect:addinstance'] = '新增「資訊重新引導」區塊';
 $string['profile_redirect:myaddinstance'] = '新增「資訊引導」區塊的例項至我的頁面';
