40c897d985205664df217ed5ef70bf2b4ae04673
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2025-10-16 21:48:57 +0000

    INT-21443: Importing da lang

--- lang/da/block_profile_redirect.php
+++ lang/da/block_profile_redirect.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @copyright  Copyright (c) 2024 Open LMS (https://www.openlms.net)
+ * @copyright  Copyright (c) 2025 Open LMS (https://www.openlms.net)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -24,19 +24,26 @@ defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Omdiriger profil';
 $string['blockname'] = 'Omdiriger profil';
 $string['description'] = 'Beskrivelse';
-$string['descriptiontxt'] = '<p>Blokken Omdirigér profil er beregnet til at omdirigere en bruger fra webstedets forside eller den Min Moodle-side, der indeholder blokken til et nyt kursus. Det nye kursus, som brugeren omdirigeres til, er baseret på en værdi i et profilfelt, der matcher en værdi inden for kursets fulde navn, korte navn eller id-nummer, afhængigt af de nedenstående webstedskonfigurationer.</p>
+$string['descriptiontxt'] = '<p>Blokken Omdirigér profil er beregnet til at omdirigere en bruger fra webstedets forside eller den Min Moodle-side, der indeholder blokken til
+et nyt kursus. Det nye kursus, som brugeren omdirigeres til, er baseret på en værdi i et profilfelt, der matcher en værdi inden for kursets fulde navn, korte navn eller id-nummer afhængigt af de nedenstående webstedskonfigurationer.</p>
 
-<p>Husk, at når blokindstillingerne er blevet konfigureret, skal en administrator tilføje en forekomst af blokken på forsiden eller Min Moodle-side. For at gøre dette skal du udføre følgende trin:<ol>
+<p>Husk, at når blokindstillingerne er konfigureret, skal en administrator tilføje en forekomst af blokken på forsiden eller Min
+Moodle-siden. For at gøre dette skal du udføre følgende trin:
+<ol>
 <li>Navigér til forsiden, aktivér redigering, og vælg Omdirigér profil fra rullemenuen "Tilføj en blok".</li>
-<li>Når blokken er blevet tilføjet, omdirigeres alle brugere til det kursus, der er angivet i deres profilfelt eller til standardkurset, hvis deres profil ikke indeholder en gyldig værdi.</li>
+<li>Når blokken er tilføjet, omdirigeres alle brugere til det kursus, der er angivet i deres profilfelt eller til standardkurset, hvis
+deres profil ikke indeholder en gyldig værdi.</li>
 </ol>
 </p>';
 $string['profilefield'] = 'Profilfelt';
-$string['profilefielddesc'] = 'Dette er feltet i en brugers profil, som blokken Omdiriger profil bruger til at matche i forhold til kursusfeltet.';
+$string['profilefielddesc'] = 'Profilfeltet er det felt i en brugers profil, som blokken Omdirigér profil bruger til at matche i forhold til
+kursusfeltet.';
 $string['coursefield'] = 'Kursusfelt';
-$string['coursefielddesc'] = 'Dette er feltet i kursusindstillingerne, som blokken Omdiriger profil bruger til at matche i forhold til brugerens profilfelt til omdirigering.';
+$string['coursefielddesc'] = 'Kursusfelt er det felt under kursusindstillingerne, som blokken Omdirigér profil bruger til at matche i forhold til
+brugerens profilfelt til omdirigering.';
 $string['defaultcourse'] = 'Standardkursus';
-$string['defaultcoursedesc'] = 'Dette er kurset, som alle brugere, der ikke er administratorbrugere, dirigeres til ved visning af blokken, hvis de ikke har en gyldig værdi i deres profilfelt.  Dette felt skal have kursets korte navn, fulde navn eller kursus-id baseret på værdien i kursusfeltets rullemenu';
+$string['defaultcoursedesc'] = 'Dette er kurset, som alle brugere, der ikke er administratorer, vil blive sendt videre til, hvis
+deres profilfelt ikke indeholder en gyldig værdi. Dette felt skal indeholde kursets korte navn, fulde navn eller kursus-id afhængigt af værdien i kursusfeltets rullemenu';
 $string['sysadmin'] = 'Denne blok vises kun for webstedsadministratorer';
 $string['profile_redirect:addinstance'] = 'Tilføj en ny blok til Omdiriger profil';
 $string['profile_redirect:myaddinstance'] = 'Føj en forekomst af blokken Omdiriger profil til siden My Moodle';
